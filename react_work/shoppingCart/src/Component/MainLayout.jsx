63b1debd5e0ea3c387cbb2dbb7272beb994cc036
Nikhil7427 <nikhilmaurya1010@gmail.com>
2025-11-10 13:02:11 +0530

    ReactRouterDom

---
+++ react_work/shoppingCart/src/Component/MainLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MainLayout() {
+  return (
+    <div>
+      <h2>Mainlayout</h2>
+    </div>
+  )
+}
+
+export default MainLayout
--- react_work/src/component/ImageManipulation.jsx
+++ react_work/src/component/ImageManipulation.jsx
@@ -6,6 +6,12 @@ export default function ImageManipulation() {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
+  const [imgAngle, setImgAngle] = useState(0);
+
+  function imgRotate(){
+    // alert("Hiii...")
+    setImgAngle(imgAngle+30);
+  }
 
   function setColor() {
     setRed(Math.random() * 255);
@@ -18,13 +24,14 @@ export default function ImageManipulation() {
       ImageManipulation
 
       <div style={{ margin: '0px auto', border: '2px solid black', height: '450px', width: '450px', paddingLeft: '300px', paddingTop: '100px' }}>
-        <img src={im} height={height} width={200} alt="image" style={{ backgroundColor: `rgb(${red}, ${green}, ${blue})` }} />
+        <img src={im} height={height} width={200} alt="image" style={{transform:`rotate(${imgAngle}deg)` , backgroundColor: `rgb(${red}, ${green}, ${blue})`}} />
       </div>
 
       <div>
         <button onClick={() => setHeight(height + 20)}>Increase Height</button>
         <button onClick={() => setHeight(height - 20)}>decrease Height</button>
         <button onClick={setColor}>ChangeBackgroundColor</button>
+        <button onClick={imgRotate}>ImageRotate</button>
       </div>
 
     </div>
